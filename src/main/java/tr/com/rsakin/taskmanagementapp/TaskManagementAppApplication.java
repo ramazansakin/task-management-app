0f2ce379c718ae3b94495516b2fb5359d34b1ecf
rsakin <ramazansakin63@gmail.com>
2025-03-22 08:42:43 +0300

    feat: Initializing the app with related dependencies

---
+++ src/main/java/tr/com/rsakin/taskmanagementapp/TaskManagementAppApplication.java
@@ -0,0 +1,13 @@
+package tr.com.rsakin.taskmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tr/com/rsakin/taskmanagementapp/TaskManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.rsakin.taskmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
